15f801f25c277019e1f4211c44bf06e42b6c95db
VladyslavSkoryk <vladyslav.skoryk.ki.2021@lpnu.ua>
2024-11-21 17:52:45 +0200

    Revert "Lab3"
    This reverts commit eb374f5331c5a074e7b7ed160021ec6d776d2a14.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lot_flutter_lab3/app.dart';
 
+import 'package:lot_flutter_lab3/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
